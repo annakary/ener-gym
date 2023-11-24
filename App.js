768ea160cc4d835afb455643761f69acd5823967
Erubiel <ero2_20@alu.uabcs.mx>
2023-11-24 16:50:50 -0700

    Cambios

--- App.js
+++ App.js
@@ -1,16 +1,6 @@
 import React from 'react';
-<<<<<<< HEAD
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-=======
-import { StyleSheet, Text, View } from 'react-native';
-import LoginScreen from './LoginScreen'; 
-import WelcomeScreen from './WelcomeScreen'
-import HomeScreen from './HomeScreen';
-import UserScreen from './UserScreen';
-import LayoutDashScreen from './LayoutDashScreen'
-import tw from 'twrnc';
->>>>>>> a6556ebe323126bde9917f74d0b9683275a5771e
 
 
 import LoginScreen from './LoginScreen';
@@ -23,7 +13,6 @@ const Stack = createStackNavigator();
 
 function App() {
   return (
-<<<<<<< HEAD
     <NavigationContainer>
       <Stack.Navigator initialRouteName="LoginScreen">
         <Stack.Screen name="LoginScreen" component={LoginScreen} />
@@ -33,15 +22,6 @@ function App() {
         <Stack.Screen name="SubscriptionScreen" component={SubscriptionScreen} />
       </Stack.Navigator>
     </NavigationContainer>
-=======
-    <View style={StyleSheet.compose(tw`flex-1 justify-center`, styles.container)}>
-      {/* <LoginScreen /> */}
-      {/* <WelcomeScreen/> */}
-      {/* <HomeScreen/> */}
-      {/* <UserScreen/> */}
-      < LayoutDashScreen/>
-    </View>
->>>>>>> a6556ebe323126bde9917f74d0b9683275a5771e
   );
 }
 
