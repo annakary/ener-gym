17a78d46b19691b25987f91be2722d373325b58a
annakary <59904505+annakary@users.noreply.github.com>
2023-11-24 14:20:14 -0700

    UserScreen completada

--- App.js
+++ App.js
@@ -3,14 +3,16 @@ import { StyleSheet, Text, View } from 'react-native';
 import LoginScreen from './LoginScreen'; 
 import WelcomeScreen from './WelcomeScreen'
 import HomeScreen from './HomeScreen';
+import UserScreen from './UserScreen';
 import tw from 'twrnc';
 
 export default function App() {
   return (
     <View style={StyleSheet.compose(tw`flex-1 justify-center`, styles.container)}>
-      <LoginScreen />
+      {/* <LoginScreen /> */}
       {/* <WelcomeScreen/> */}
       {/* <HomeScreen/> */}
+<UserScreen/>
     </View>
   );
 }
