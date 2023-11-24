51adc49b3153f4eff0c6ae54a29bd5b2da316fe3
annakary <59904505+annakary@users.noreply.github.com>
2023-11-23 23:40:54 -0700

    Añadi esqueleto a componentes

---
+++ UserScreen.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity } from 'react-native';
+import tw from 'twrnc';
+
+const UserScreen = () => {
+  
+
+  return (
+    <View style={tw`flex-1 justify-center items-center bg-gray-900`}>
+      
+    </View>
+  );
+};
+
+export default UserScreen;
